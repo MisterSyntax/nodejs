72e9b120041545fdbd62d42d356d9fa41c5e86bf
Andrew Syriala <asyriala@overstock.com>
2019-01-10 23:27:34 -0700

    Removed hateful console

--- src/server.js
+++ src/server.js
@@ -1,20 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
-import { template } from './index.js';
+import {
+    template } from './index.js';
 
 const app = express();
-
-console.log(`
-
-
-
-
-${template}
-
-
-
-
-`);
 
 app.get('/', (req, res) => res.send(template));
 app.listen(process.env.PORT,
